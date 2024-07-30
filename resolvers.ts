d85591a1a411a5cfd77148da8190232c6ea23eb7
Xuan Nam <leeminhnam2k2@gmail.com>
2024-07-30 13:03:05 +0700

    feature to get and create a article

--- resolvers.ts
+++ resolvers.ts
@@ -1,15 +1,35 @@
 import Article from "./model/articles.model"
 
-// Bên resolvers chứa những hàm xử lý logic
+// Bên resolvers chứa những hàm xử lý logic (same same folder controller)
 export const resolvers = {
+  // Xứ lý logic các hàm [GET]
   Query: {
     hello: () => {
       return "hello World"
     },
     getlistArticle: async () => {
-      const record = await Article.find({
+      const articles = await Article.find({
         deleted: false
       });
+      return articles;
+    },
+    getArticle: async (_, agrs) => {
+      const { id } = agrs; // agrs : các đối số được font-end gửi lên
+      const article = await Article.findOne({
+        _id: id,
+        deleted: false
+      });
+      return article;
+    }
+  },
+  // Xử lý logic các hàm [POST, PATCH, DELETE, PUT]
+  Mutation: {
+    createArticle: async (_, agrs) => {
+      const { article } = agrs;
+
+      const record = new Article(article);
+      await record.save();
+
       return record;
     }
   }
--- type-defs.ts
+++ type-defs.ts
@@ -1,17 +1,28 @@
 import { gql } from "apollo-server-express";
 
 // Bên typeDefs dùng để định nghĩa các key cho phép người dùng được phép lấy
-// type Query chứa các câu lệnh để người ta có thẻ truy vấn ra được
 export const typeDefs = gql`
-  type Articles {
-    id: ID,
-    title: String,
+  type Article {
+    id: ID, # returns a ID
+    title: String, # returns a String
     avatar: String,
     description: String
   }
+
+  # type Query chứa các câu lệnh để người dùng truy vấn dữ liệu [GET]
   type Query {
     hello: String,
-    getlistArticle: [Articles],
-
+    getlistArticle: [Article], # returns a array Article
+    getArticle(id: ID): Article # returns a Article
+  }
+  # Khởi tạo các giá trị người dùng có thể truyền vào 
+  input ArticleInput {
+    title: String, # returns a String
+    avatar: String,
+    description: String
+  }
+  # type Mutaiton chứa các câu lện để người dùng thêm, sửa, xoá ,... [POST, PATCH, DELETE, PUT]
+  type Mutation {
+    createArticle(article: ArticleInput): Article # return a Article
   }
 `
